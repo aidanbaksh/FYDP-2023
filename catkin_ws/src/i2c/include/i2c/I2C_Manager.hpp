de6b578636df970e83988bcd54082a5fcf9969d3
captsone <maaranmurugathas@crescentschool.org>
2023-03-01 16:21:40 -0500

    Mostly working IMU. Still needs filtering due to spurious 0 readings

--- catkin_ws/src/i2c/include/i2c/I2C_Manager.hpp
+++ catkin_ws/src/i2c/include/i2c/I2C_Manager.hpp
@@ -15,45 +15,59 @@ inline static const std::string NODE_NAME = "i2c_manager"; // must be extern to 
 
 
 namespace imu_corrections {
-    // acceleration data correction
-    static constexpr int16_t ACCEL_X = -250;
-    static constexpr int16_t ACCEL_Y = 36;
-    static constexpr int16_t ACCEL_Z = 1200;
+    // NOTE: these correction factors depend on the full scale range of each sensor
+    // https://github.com/adafruit/Adafruit_MPU6050/blob/master/Adafruit_MPU6050.cpp#L660-L689
+    // Accelerometer scale is +-2g
+    // Gyroscope scale is +- 250 deg/s
 
-    // temperature correction
-    static constexpr int16_t TEMP = -1400;
+    static constexpr double ACCEL_SCALE = 16384;
+    static constexpr double GYRO_SCALE = 131;
 
-    // gyro correction
-    static constexpr int16_t GYRO_X = -335;
-    static constexpr int16_t GYRO_Y = 250;
-    static constexpr int16_t GYRO_Z = 170;
+    static constexpr double SENSORS_GRAVITY_STANDARD = 9.80665;
+    static constexpr double SENSORS_DPS_TO_RADS = M_PI / 180.0;
 };
 
 
 namespace addr {
-    static constexpr unsigned int ARDUINO = 0x10;
+    namespace arduino {
+        static constexpr unsigned int DEVICE = 0x10;
+    };
 
     namespace imu {
         static constexpr unsigned int DEVICE = 0x68;
 
+        // See internal register map
+        // https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
         namespace internal {
-            static constexpr unsigned int ACCEL = 0x3B;
-            static constexpr unsigned int GYRO = 0x43;
-            static constexpr unsigned int RESET = 0x6B;
+            // used for device reset and clock select
+            static constexpr unsigned int PWR_MGMT_1 = 0x6B;
+            // used to reset individual sensors and initialize serial interface
+            static constexpr unsigned int SIGNAL_PATH_RESET = 0x68;
+            // used to reset individual sensors and initialize serial interface
+            static constexpr unsigned int SMPRT_DIV = 0x19;
+            // configures digital LPF for both accelerometer and gyroscope
+            static constexpr unsigned int CONFIG = 0x1A;
+            // configures scale of accelerometer
             static constexpr unsigned int ACCEL_CONFIG = 0x1C;
+            // configures scale of gyro
             static constexpr unsigned int GYRO_CONFIG = 0x1B;
+
+            // technically the address of ACCEL_XOUT_H but we read all 6 ACCEL_OUT registers at a time
+            static constexpr unsigned int ACCEL_OUT = 0x3B;
+            // technically the address of GYRO_XOUT_H but we read all 6 ACCEL_OUT registers at a time
+            static constexpr unsigned int GYRO_OUT = 0x43;
         };
     };
 };
 
 
 class I2C_Manager {
-    static constexpr size_t PUBLISH_INTERVAL_MS = 100;
+    static constexpr size_t PUBLISH_INTERVAL_MS = 20; // 20Hz
 
     static constexpr unsigned int BUS_NUM = 0;
     inline static const std::string BUS_NAME = "/dev/i2c-" + std::to_string(I2C_Manager::BUS_NUM);
 
-    static constexpr size_t NUM_ULTRASONICS = 2; // 1 per ultrasonic
+    static constexpr size_t NUM_ULTRASONICS = 2;
     static const std::array<std::string, NUM_ULTRASONICS> ULTRASONIC_TOPICS;
 
     static constexpr size_t NUM_IMU_AXES = 3;
@@ -87,25 +101,31 @@ private:
     // thread that actually communicates with devices
     void get_data();
 
-
     // convert acceleration to angle
     std::pair<double, double> get_angle(const int16_t, const int16_t, const int16_t) const;
 
+    // ROS node handle
     ros::NodeHandle& nh;
 
+    // flags for coordination between i2c reader thread and ROS timer callback
     std::atomic_flag data_available, stopped;
     ros::Timer timer;
+
+    // ROS publishers
     std::array<ros::Publisher, NUM_ULTRASONICS> ultrasonic_publishers;
     ros::Publisher imu_publisher;
 
+    // i2c bus file descriptor and devices
     int bus_fd;
     I2CDevice arduino, imu;
 
+    // buffer of ultrasonic readings
     int32_t ultrasonic_buffer[NUM_ULTRASONICS];
-
-    int16_t imu_buffer[NUM_IMU_AXES];
-    std::tuple<int16_t, int16_t, int16_t> imu_accel;
-    std::tuple<int16_t, int16_t, int16_t> imu_gyro;
+    // buffer used to read accel and gyro
+    // each reading is stored in 2 registers, so we have to read them individual then combine
+    int8_t imu_buffer[NUM_IMU_AXES*2];
+    std::tuple<double, double, double> imu_accel; // reading in m/s^2
+    std::tuple<double, double, double> imu_gyro; // reading in rad/s
 
     std::thread device_reader;
 };
--- catkin_ws/src/i2c/src/I2C_Manager.cpp
+++ catkin_ws/src/i2c/src/I2C_Manager.cpp
@@ -18,12 +18,7 @@ I2C_Manager::I2C_Manager(ros::NodeHandle& nh):
     nh(nh),
     data_available(ATOMIC_FLAG_INIT),
     stopped(ATOMIC_FLAG_INIT),
-    timer(
-        nh.createTimer(
-            ros::Duration(I2C_Manager::PUBLISH_INTERVAL_MS/1000.0),
-            &I2C_Manager::timer_callback, this
-        )
-    ),
+    timer(),
     ultrasonic_publishers(),
     bus_fd(-1),
     arduino(),
@@ -69,7 +64,7 @@ void I2C_Manager::init() {
     }
 
     // initialize device structs
-    init_device(arduino, addr::ARDUINO, 0);
+    init_device(arduino, addr::arduino::DEVICE, 0);
     init_device(imu, addr::imu::DEVICE, 1);
 
     // print i2c device descriptions
@@ -77,12 +72,36 @@ void I2C_Manager::init() {
     ROS_INFO_STREAM("Arduino: " << i2c_get_device_desc(&arduino, i2c_dev_desc, sizeof(i2c_dev_desc)));
     ROS_INFO_STREAM("IMU: " << i2c_get_device_desc(&imu, i2c_dev_desc, sizeof(i2c_dev_desc)));
 
+    // configuration is mostly based on Adafruit library
+    // https://github.com/adafruit/Adafruit_MPU6050/blob/master/Adafruit_MPU6050.cpp#L105-L141
+
     // reset the imu and disable the temperature sensor
-    configure_imu(addr::imu::internal::RESET, 0b10001000);
-    // set the accelerometer scale to +- 4g
-    configure_imu(addr::imu::internal::ACCEL_CONFIG, 0b00010000);
+    configure_imu(addr::imu::internal::PWR_MGMT_1, 0b10001000);
+    // reset individual sensors and initialize serial interface
+    configure_imu(addr::imu::internal::SIGNAL_PATH_RESET, 0b00000111);
+    // set sample rate divisor, converts clock rate into measurement rate
+    // TODO: this will depend on our clock rate
+    configure_imu(addr::imu::internal::SMPRT_DIV, 0);
+
+    // TODO: this got changed
+    // set filter bandwidth to 44Hz for accel and 42Hz for gyro
+    // ROS node runs at 20 Hz so this rejects anything faster without introducing delay
+    configure_imu(addr::imu::internal::CONFIG, 0b00000000);
+
     // set the gyro scale to 250 deg/s
     configure_imu(addr::imu::internal::GYRO_CONFIG, 0b00000000);
+    // set the accelerometer scale to +- 2g
+    configure_imu(addr::imu::internal::ACCEL_CONFIG, 0b00000000);
+    // set clock config to PLL with Gyro X reference
+    configure_imu(addr::imu::internal::PWR_MGMT_1, 0b00000001);
+
+    ros::Duration(0.1).sleep(); // sleep for 100ms
+
+    // setup publish callback now that timer
+    timer = nh.createTimer(
+        ros::Duration(I2C_Manager::PUBLISH_INTERVAL_MS/1000.0),
+        &I2C_Manager::timer_callback, this
+    );
 
     // unblock the device_reader thread now that the i2c bus is initialized
     data_available.clear(std::memory_order_seq_cst);
@@ -117,9 +136,9 @@ void I2C_Manager::timer_callback(const ros::TimerEvent&) {
     imu_publisher.publish(imu_msg);
 
     // print data
-    std::cout << "Angle: Pitch = " << angle.first << " | Roll = " << angle.second << std::endl;
-    std::cout << "Accelerometer: X = " << std::get<0>(imu_accel) << " | Y = " << std::get<1>(imu_accel) << " | Z = " << std::get<2>(imu_accel) << std::endl;
-    std::cout << "Gyroscope: X = " << std::get<0>(imu_gyro) << " | Y = " << std::get<1>(imu_gyro) << " | Z = " << std::get<2>(imu_gyro) << std::endl;
+    // std::cout << "Angle: Pitch = " << angle.first << " | Roll = " << angle.second << std::endl;
+    // std::cout << "Accelerometer: X = " << std::get<0>(imu_accel) << " | Y = " << std::get<1>(imu_accel) << " | Z = " << std::get<2>(imu_accel) << std::endl;
+    // std::cout << "Gyroscope: X = " << std::get<0>(imu_gyro) << " | Y = " << std::get<1>(imu_gyro) << " | Z = " << std::get<2>(imu_gyro) << std::endl;
 
     // consumed the data, no longer available
     data_available.clear();
@@ -137,34 +156,40 @@ void I2C_Manager::get_data() {
         }
 
         // read imu acceleration 
-        if (!read_device(imu, addr::imu::internal::ACCEL, imu_buffer)) {
+        if (!read_device(imu, addr::imu::internal::ACCEL_OUT, imu_buffer)) {
             ROS_ERROR("i2c error when reading imu acceleration!\n");
         }
         imu_accel = std::make_tuple(
-            imu_buffer[0] + imu_corrections::ACCEL_X,
-            imu_buffer[1] + imu_corrections::ACCEL_Y,
-            imu_buffer[2] + imu_corrections::ACCEL_Z
+            double(imu_buffer[0] << 8 | imu_buffer[1]) / imu_corrections::ACCEL_SCALE * imu_corrections::SENSORS_GRAVITY_STANDARD,
+            double(imu_buffer[2] << 8 | imu_buffer[3]) / imu_corrections::ACCEL_SCALE * imu_corrections::SENSORS_GRAVITY_STANDARD,
+            double(imu_buffer[4] << 8 | imu_buffer[5]) / imu_corrections::ACCEL_SCALE * imu_corrections::SENSORS_GRAVITY_STANDARD
         );
 
         // read imu gyro
-        if (!read_device(imu, addr::imu::internal::GYRO, imu_buffer)) {
+        if (!read_device(imu, addr::imu::internal::GYRO_OUT, imu_buffer)) {
             ROS_ERROR("i2c error when reading imu gyro!\n");
         }
         imu_gyro = std::make_tuple(
-            imu_buffer[0] + imu_corrections::GYRO_X,
-            imu_buffer[1] + imu_corrections::GYRO_Y,
-            imu_buffer[2] + imu_corrections::GYRO_Z
+            (imu_buffer[0] << 8 | imu_buffer[1]) / imu_corrections::GYRO_SCALE * imu_corrections::SENSORS_DPS_TO_RADS,
+            (imu_buffer[2] << 8 | imu_buffer[3]) / imu_corrections::GYRO_SCALE * imu_corrections::SENSORS_DPS_TO_RADS,
+            (imu_buffer[4] << 8 | imu_buffer[5]) / imu_corrections::GYRO_SCALE * imu_corrections::SENSORS_DPS_TO_RADS
         );
+
+        std::cout
+            << std::fixed
+            << std::setprecision(2)
+            << "IMU: ("
+            << std::get<0>(imu_accel) << ", "
+            << std::get<1>(imu_accel) << ", "
+            << std::get<2>(imu_accel) << ")\t\tGyro: ("
+            << std::get<0>(imu_gyro) << ", "
+            << std::get<1>(imu_gyro) << ", "
+            << std::get<2>(imu_gyro) << ")"
+            << std::endl;
 
         // std::cout << "Got ultrasonic data:" << std::endl;
         // for (size_t i = 0; i < NUM_ULTRASONICS; ++i) {
         //     std::cout << ultrasonic_buffer[i] << " ";
-        // }
-        // std::cout << std::endl;
-
-        // std::cout << "Got imu data:" << std::endl;
-        // for (size_t i = 0; i < NUM_IMU_READINGS; ++i) {
-        //     std::cout << imu_buffer[i] << " ";
         // }
         // std::cout << std::endl;
 
