9d40cff515c5f02e25a83d4fea95696196777ed2
captsone <maaranmurugathas@crescentschool.org>
2023-03-03 17:30:27 -0500

    playing sound, need to edit string generation

--- catkin_ws/src/audio_feedback/src/audio_feedback_node.py
+++ catkin_ws/src/audio_feedback/src/audio_feedback_node.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import time
 from enum import Enum
 
 import rospy
@@ -53,7 +52,13 @@ class SoundManager:
         rospy.sleep(0.5)
 
         self._last_played_dict = {}
-        self._sev_time_delays = {Severity.LOW: 10, Severity.MEDIUM: 5, Severity.HIGH: 0}
+        self._node_start_time = rospy.Time.now()
+
+        self._sev_time_delays = {
+            Severity.LOW: rospy.Duration(10),
+            Severity.MEDIUM: rospy.Duration(5),
+            Severity.HIGH: rospy.Duration(0)
+            }
     
     def _audio_warning_callback(self, msg: AudioWarning) -> None:
         h_type = msg.warning_type
@@ -91,7 +96,7 @@ class SoundManager:
     def checkTimeDelay(self, sev: Severity) -> bool:
         delay = self._sev_time_delays[sev]
         last_time = self.getLastPlayedTime(self._request_path)
-        cur_time = time.time()
+        cur_time = rospy.Time.now()
         if cur_time-last_time > delay :
             return True
         return False
@@ -100,39 +105,45 @@ class SoundManager:
         if path in self._last_played_dict.keys():
             return self._last_played_dict[path]
         else:
-            return 0
+            return self._node_start_time
 
     def setLastPlayedTime(self, path):
-        cur_time = time.time() ## Modify to get the time from ROS
-        self._last_played_dict[path] = cur_time
+        self._last_played_dict[path] = rospy.Time.now()
 
-    def setPathFromInputs(self, h_type: HazardType, dir: Direction) -> bool:
+    def setPathFromInputs(self, h_type: HazardType, dir: Direction) -> None:
         path = "audio_warnings/"
 
-        match h_type:
-            case HazardType.CURB: path += "curb/curb"
-            case HazardType.OBJECT: path += "object/object"
-            case HazardType.TIP: path += "tip/tip"
-            case _: return False
+        if h_type == HazardType.CURB:
+            path += "curb/curb"
+        elif h_type == HazardType.OBJECT:
+            path += "object/object"
+        elif h_type == HazardType.TIP:
+            path += "tip/tip"
         
-        match dir:
-            case Direction.FRONT: path += "_front"
-            case Direction.FRONT_RIGHT: path += "_front_right"
-            case Direction.RIGHT: path += "_right"
-            case Direction.BACK_RIGHT: path += "_back_right"
-            case Direction.BACK: path += "_back"
-            case Direction.BACK_LEFT: path += "_back_left"
-            case Direction.LEFT: path += "_left"
-            case Direction.FRONT_LEFT: path += "_front_left"
-            case _: return False
+        if dir == Direction.FRONT:
+            path += "_front"
+        elif dir == Direction.FRONT_RIGHT: 
+            path += "_front_right"
+        elif dir == Direction.RIGHT: 
+            path += "_right"
+        elif dir == Direction.BACK_RIGHT: 
+            path += "_back_right"
+        elif dir == Direction.BACK: 
+            path += "_back"
+        elif dir == Direction.BACK_LEFT: 
+            path += "_back_left"
+        elif dir == Direction.LEFT: 
+            path += "_left"
+        elif dir == Direction.FRONT_LEFT: 
+            path += "_front_left"
 
         self._request_path = path
-        return True
         
         
 
 if __name__ == '__main__':
     manager = SoundManager()
+    rospy.spin()
 
 
 
